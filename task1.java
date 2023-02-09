69962c3376bbf7aa5fb807259116a0a16ab286ce
odinnone <vladborovkov01@mail.ru>
2023-02-09 16:28:15 +0300

    add task1

--- task1.java
+++ task1.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
-public class task {
+public class task1 {
     public static void main(String[] args) {
         Scanner Scanner = new Scanner(System.in);
-        System.out.print("Введите число : ");
+        System.out.print("Введите число: ");
         int a = Scanner.nextInt();
         Scanner.close();
-        System.out.println("Треугольное число = " + Triangle(a) + "\n");
+        System.out.println("Треугольное число: " + Triangle(a) + "\n");
     }
     public static int Triangle(int num) {
         if (num == 1) {
