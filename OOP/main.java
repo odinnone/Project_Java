a4af7516c7f69d997dc5a7eb98094aa98169392f
odinnone <vladborovkov01@mail.ru>
2023-03-03 19:24:43 +0300

    remake main

--- OOP/main.java
+++ OOP/main.java
@@ -2,13 +2,25 @@ import Heroes.*;
 
 public class main {
     public static void Main(String[] args) {
-        magician hero1 = new magician();
+        BaseHero hero1 = new magician();
         System.out.println(hero1.getInfo());
         
-        monk hero2 = new monk();
+        BaseHero hero2 = new peasant();
         System.out.println(hero2.getInfo());
 
-        monk hero3 = new monk();
+        BaseHero hero3 = new spearman();
         System.out.println(hero3.getInfo());
+
+        BaseHero hero4 = new outlaw();
+        System.out.println(hero4.getInfo());
+
+        BaseHero hero5 = new sniper();
+        System.out.println(hero5.getInfo());
+
+        BaseHero hero6 = new crossbowman();
+        System.out.println(hero6.getInfo());
+
+        BaseHero hero7 = new monk();
+        System.out.println(hero7.getInfo());
     }
 }
