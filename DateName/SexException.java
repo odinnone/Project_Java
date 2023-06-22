643097245921e43ef8587227fea9aa2af46645dc
odinnone <vladborovkov01@mail.ru>
2023-06-22 14:35:20 +0300

    add DateName

---
+++ DateName/SexException.java
@@ -0,0 +1,12 @@
+package DateName;
+
+public class SexException extends Exception {
+    public SexException() {
+    }
+
+    public void sexException(String i) {
+        System.out.println("Exception: SexNotCorrectData");
+        System.out.printf("It`s not correct data: %s", i);
+        System.out.println();
+    }
+}
